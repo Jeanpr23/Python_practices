d9083e95ddfca65a63ea45bbca4fdb799712bdea
Jean Paul <jeanpauldavidaponte@gmail.com>
2026-08-06 18:45:47 -0400

    Update streamlit_app.py

--- AudioMaker/streamlit_app.py
+++ AudioMaker/streamlit_app.py
@@ -22,10 +22,13 @@ clean_folder("uploads", 24)
 clean_folder("generated", 24)
 
 import os
+import imageio_ffmpeg
 
 os.makedirs("uploads", exist_ok=True)
 os.makedirs("generated", exist_ok=True)
 
+st.write(FFmpeg:", imageio_ffmpeg.get_ffmpeg_exe())
+st.write("Exists", os.path.exists(imageio_ffmpeg.get_ffmpeg_exe()))
 
 st.set_page_config(
     page_title="Manipulation",
