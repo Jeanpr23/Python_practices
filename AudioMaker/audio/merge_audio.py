8154fa0f882ecbd1fef40b4238a0da115dbbb0a7
Jean Paul <jeanpauldavidaponte@gmail.com>
2026-08-07 19:57:54 -0400

    Update merge_audio.py

--- AudioMaker/audio/merge_audio.py
+++ AudioMaker/audio/merge_audio.py
@@ -1,34 +1,30 @@
 import subprocess
 import imageio_ffmpeg
 
+
 def merge_audio(tts_file, user_file, output_file):
 
     ffmpeg = imageio_ffmpeg.get_ffmpeg_exe()
 
     command = [
         ffmpeg,
-        "-y",
         "-i", tts_file,
         "-i", user_file,
         "-filter_complex",
-        "[0:a:0][1:a:0]concat=n=2:v=0:a=1[out]",
+        "[0:a][1:a]concat=n=2:v=0:a=1[out]",
         "-map", "[out]",
-        "-c:a", "libmp3lame",
+        "-y",
         output_file
-
     ]
 
     result = subprocess.run(
-         command,
-         stdout=subprocess.PIPE,
-         stderr=subprocess.PIPE,
-         text=True
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
     )
 
     if result.returncode != 0:
         raise RuntimeError(
-            "FFmpeg command:\n"
-            + " ".join(command)
-            + "\n\nFFmpeg error:\n"
-            + result.stderr
+            f"FFmpeg error:\n{result.stderr}"
         )
