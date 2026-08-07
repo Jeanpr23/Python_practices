1b6fb76ee330694725163bb56ff58cdfb50e9be2
Jean Paul <jeanpauldavidaponte@gmail.com>
2026-08-06 23:35:58 -0400

    Update merge_audio.py

--- AudioMaker/audio/merge_audio.py
+++ AudioMaker/audio/merge_audio.py
@@ -1,16 +1,23 @@
 from pydub import AudioSegment
 import imageio_ffmpeg
 from pydub import utils
+import shutlil
 
 ffmpeg = imageio_ffmpeg.get_ffmpeg_exe()
 
 AudioSegment.converter = ffmpeg
 utils.get_prober_name = lambda: ffmpeg
 
+ffprobe = shutlil.wich("ffprobe")
+
+if ffprobe:
+    AudioSegment.ffprobe = ffprobe
+    
 def merge_audio(tts_file, user_file, output_file):
 
     tts_audio = AudioSegment.from_file(
-        tts_file
+        tts_file,
+        format="mp3"
     )
 
     user_audio = AudioSegment.from_file(
