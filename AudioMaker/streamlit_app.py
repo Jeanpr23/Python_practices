4337f6313929990832dc7ec4ba9f3f3621a77f6c
Jean Paul <jeanpauldavidaponte@gmail.com>
2026-08-06 22:28:53 -0400

    Update streamlit_app.py

--- AudioMaker/streamlit_app.py
+++ AudioMaker/streamlit_app.py
@@ -27,9 +27,6 @@ import imageio_ffmpeg
 os.makedirs("uploads", exist_ok=True)
 os.makedirs("generated", exist_ok=True)
 
-st.write("FFmpeg:", imageio_ffmpeg.get_ffmpeg_exe())
-st.write("Exists:", os.path.exists(imageio_ffmpeg.get_ffmpeg_exe()))
-
 st.set_page_config(
     page_title="Manipulation",
     page_icon="🗣️",
